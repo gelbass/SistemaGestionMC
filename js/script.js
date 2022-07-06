10e2e894df09b2a121693ec8157891bd381da9ed
gelbass <lacruz.german@gmail.com>
2022-07-06 00:09:20 -0300

    Insertar datos a tablas desde js

--- js/script.js
+++ js/script.js
@@ -11,6 +11,7 @@ let numero = 0;
 let costo = 0.0;
 const listadoMateriaPrima = [];
 const listadoProducto = [];
+const listadoPrecios = [];
 
 
 class MateriaPrima {
@@ -91,9 +92,130 @@ const costoPorIngrediente = (producto) => {
     return costoIngrediente + MANODEOBRA;
 }
 
+// CREAR TABLAS
+const constructorTablas = (array, contenedor, tipo) => {
+    switch (tipo) {
+        case 'materiaPrima':
+            let tableM = document.createElement('table');
+            let theadM = document.createElement('thead');
+            let tbodyM = document.createElement('tbody');
+            tableM.appendChild(theadM);
+            tableM.appendChild(tbodyM);
+            tableM.className = 'table';
+            document.getElementById(contenedor).appendChild(tableM);
+
+            let tituloM = document.createElement('tr');
+            let tM1 = document.createElement('th');
+            let tM2 = document.createElement('th');
+            let tM3 = document.createElement('th');
+
+            tM1.innerHTML = 'Materia Prima';
+            tM2.innerHTML = 'Cant. Empaque';
+            tM3.innerHTML = 'Costo';
+
+            tituloM.appendChild(tM1);
+            tituloM.appendChild(tM2);
+            tituloM.appendChild(tM3);
+            theadM.appendChild(tituloM);
+
+            for (const elemento of array) {
+                let rowM = document.createElement('tr');
+                let colM1 = document.createElement('td');
+                colM1.innerHTML = elemento.materiaPrima;
+                let colM2 = document.createElement('td');
+                colM2.innerHTML = elemento.cantidadEmpaque;
+                let colM3 = document.createElement('td');
+                colM3.innerHTML = elemento.costo;
+
+                rowM.appendChild(colM1);
+                rowM.appendChild(colM2);
+                rowM.appendChild(colM3);
+                tbodyM.appendChild(rowM);
+            }
+            break;
+
+        case 'producto':
+            for (const elemento of listadoProducto) {
+                let producto = document.createElement('h3');
+                producto.innerHTML = elemento.nomProducto;
+                document.getElementById(contenedor).append(producto);
+
+                let tableP = document.createElement('table');
+                let theadP = document.createElement('thead');
+                let tbodyP = document.createElement('tbody');
+
+                tableP.appendChild(theadP);
+                tableP.appendChild(tbodyP);
+                tableP.className = 'table';
+
+                document.getElementById(contenedor).appendChild(tableP);
+
+                let tituloP = document.createElement('tr');
+                let tP1 = document.createElement('th');
+                let tP2 = document.createElement('th');
+
+                tP1.innerHTML = 'Materia Prima';
+                tP2.innerHTML = 'Cantidad';
+
+                tituloP.appendChild(tP1);
+                tituloP.appendChild(tP2);
+                theadP.appendChild(tituloP);
+
+                for (const valorElemento of elemento.ingredientes) {
+                    let rowP = document.createElement('tr');
+                    let colP1 = document.createElement('td');
+                    let colP2 = document.createElement('td');
+
+                    colP1.innerHTML = valorElemento.ingrediente.materiaPrima;
+                    colP2.innerHTML = valorElemento.cantidad;
+                    rowP.appendChild(colP1);
+                    rowP.appendChild(colP2);
+                    tbodyP.appendChild(rowP);
+                }
+            }
+            break;
+        case 'precios':
+            let table = document.createElement('table');
+            let thead = document.createElement('thead');
+            let tbody = document.createElement('tbody');
+            table.appendChild(thead);
+            table.appendChild(tbody);
+            table.className = 'table';
+            document.getElementById(contenedor).appendChild(table);
+
+            let titulo = document.createElement('tr');
+            let t1 = document.createElement('th');
+            let t2 = document.createElement('th');
+
+            t1.innerHTML = 'Producto';
+            t2.innerHTML = 'Precio de Venta';
+
+            titulo.appendChild(t1);
+            titulo.appendChild(t2);
+            thead.appendChild(titulo);
+
+            for (const producto of array) {
+                let venta = costoPorIngrediente(producto);
+                let row = document.createElement('tr');
+                let col1 = document.createElement('td');
+                col1.innerHTML = producto.nomProducto;
+                let col2 = document.createElement('td');
+                col2.innerHTML = venta.toFixed(2);
+
+                row.appendChild(col1);
+                row.appendChild(col2);
+                tbody.appendChild(row);
+            }
+            break;
+        default:
+            break;
+    }
+}
+
+// INICIO DE SIMULADOR
 while (salir) {
     alert("···· SIMULADOR DE COSTOS ····")
-    let opcion = prompt("···· SIMULADOR DE COSTOS ····\n1. Ingresar Materia Prima. \n2. Ingreso de producto a preparar. \n3. Calculo de costos.\n0. Salir del sistema.");
+    let opcion = prompt("···· SIMULADOR DE COSTOS ····\n1. Ingresar Materia Prima. \n2. Ingreso de producto a preparar. \n3. Calculo de costos.\n0. Visualizar datos.");
 
     switch (opcion) {
         case "1":
@@ -104,10 +226,15 @@ while (salir) {
                 if (!existe(listadoMateriaPrima, nomMateriaPrimaValido, true)) {
                     let cantidadEmpaque = parseInt(validarNumeros(prompt(`Producto: ${nomMateriaPrimaValido}\nIngrese cantidad del empaque:`), "ERROR: INGRESE SOLO NUMEROS\nIngrese nuevamente la cantidad del empaque:"));
                     let costoMateriaPrima = parseInt(validarNumeros(prompt(`Producto: ${nomMateriaPrimaValido}\nIngrese costo del empaque:`), "ERROR: INGRESE SOLO NUMEROS\nIngrese nuevamente el costo del empaque:"));
-
                     listadoMateriaPrima.push(new MateriaPrima(nomMateriaPrimaValido, cantidadEmpaque, costoMateriaPrima));
-
                     nuevoIngrediente = validarOpcionContinuar(prompt("¿Desea ingresar un nuevo Ingrediente?\n1 - Si.\n2 - No."), "ERROR: Opcion Incorrecta.\n¿Desea ingresar un nuevo Ingrediente?\n1 - Si.\n2 - No.");
+                    /* 
+                    for (const producto of listadoMateriaPrima) {
+                        let columna = document.createElement("td");
+                        let contenido = document.createTextNode(producto);
+                        document.querySelector("#materiales__td").columna.append(contenido);
+                    } */
+
                 } else {
                     alert(`ERROR: YA EL PRODUCTO ${nomMateriaPrimaValido} FUE AGREGADO.\nDebe ingresar un nuevo ingrediente`);
                 }
@@ -194,4 +321,7 @@ while (salir) {
             break;
     }
 }
-alert("GRACIAS POR UTILIZAR EL SISTEMA DE COSTOS");
+alert("A CONTINUACION PODRA VISUALIZAR LOS DATOS INGRESADOS");
+constructorTablas(listadoMateriaPrima, "materiales", "materiaPrima");
+constructorTablas(listadoProducto, "productos", "producto");
+constructorTablas(listadoProducto, "listaPrecios", "precios");
