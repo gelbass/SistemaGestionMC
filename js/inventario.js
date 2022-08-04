14aebbae7629d7239c1331955d327cacaccfea48
gelbass <lacruz.german@gmail.com>
2022-08-03 22:12:46 -0300

    Modificaciones 03/08

--- js/inventario.js
+++ js/inventario.js
@@ -1,4 +1,7 @@
 sessionStorage.setItem("nuevaSession", "no");
+sessionStorage.setItem("materiaPrima",localStorage.getItem("materiaPrima"));
+sessionStorage.setItem("productos",localStorage.getItem("productos"));
+sessionStorage.setItem("inventarioMp",localStorage.getItem("inventarioMp"));
 inventarioMateriaPrima = JSON.parse(sessionStorage.getItem("inventarioMp"));
 console.log(inventarioMateriaPrima);
 constructorTablas(inventarioMateriaPrima, "tbMateriales");
